390d5936d778cf46c169af2782d1f3f5d4cfb0b3
DomDom <domdom314@gmail.com>
2015-06-30 17:26:24 +1000

    Update holkins effect. Prettier trail, and more spectacular explosion.

---
+++ src/static/holkins/flicker_alpha.py
@@ -0,0 +1,28 @@
+flashes = 10
+
+start_alpha = 0.6
+end_alpha = 0.0
+
+
+o = []
+for i in range(flashes):
+	t = float(i) / flashes
+	if i % 4 == 0:
+		a = t * end_alpha + (1 - t) * start_alpha
+	else:
+		a = 0.1
+	o.append([t, a])
+
+print(o)
+
+o = []
+
+for i in range(flashes):
+	t = float(i) / flashes
+	if i % 4 == 2:
+		a = t * end_alpha + (1 - t) * start_alpha
+	else:
+		a = 0
+	o.append([t, a])
+
+print(o)
